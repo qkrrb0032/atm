a817a854b9bdfdb8d50066ca1c198d9bfa9c0ab7
qkrrb0032 <qkrrb0032@naver.com>
2023-06-22 17:15:32 +0900

    Update UserManager

--- atm/src/atm/UserManager.java
+++ atm/src/atm/UserManager.java
@@ -3,125 +3,149 @@ package atm;
 import java.util.ArrayList;
 
 public class UserManager {
-	// Design Pattern 설계 패턴중,
-	// 싱글 인스턴스를 만드는
-	// Singleton Pattern 을 사용해보자
 	
-	// 1) 생성자를 숨긴다 private
+	private AccountManager accManager = AccountManager.getInstance();
+	private Atm atm = new Atm();
+	
+	private ArrayList<User> list = new ArrayList<User>();
+	
+	// Design Pattern (GOF) 설계 패턴 중, 
+	// 싱글 인스턴스를 만드는 
+	// Singleton Pattern 을 사용해보자 
+	
+	// 1) 생성자를 숨긴다 private 
 	private UserManager() {}
 	
-	// 2) 클래스 내부에서 단일 인스턴스를 생성해준다.
+	// 2) 클래스 내부에서 단일 인스턴스를 생성해준다 
 	private static UserManager instance = new UserManager();
 	
-	// 3) 외부에서 단일 인스턴스를 참조할 수 있도록 -> getter를 제공한다.
+	// 3) 외부에서 단일 인스턴스를 참조할 수 있도록 -> getter 를 제공한다 
 	public static UserManager getInstance() {
 		return instance;
 	}
 	
-	
-	public int getLog() {
-		return this.log;
-	}
-
-	private ArrayList<User> list = new ArrayList<User>();	
-	private int log = -1;
-
-	public void joinUser() {		
-		int userCode = generateRandomCode();		
+	public void joinUser() {
+		int userCode = generateRandomCode();
 		System.out.print("id : ");
-		String id = Atm.scan.next();
-		System.out.print("password : ");		
-		String password = Atm.scan.next();
+		String id = Atm.scanner.next();
+		System.out.print("password : ");
+		String password = Atm.scanner.next();
 		System.out.print("name : ");
-		String name = Atm.scan.next();
+		String name = Atm.scanner.next();
 		
 		if(!duplId(id)) {
-			User user = new User(userCode,name,id,password);
+			User user = new User(userCode, name, id, password);
 			this.list.add(user);
+			accManager.createAccount(user);
+			
+			
 			System.out.println("회원가입 완료");
-		}else {
+		} else {
 			System.err.println("중복되는 아이디 입니다.");
 		}
+	}
+	
+	public User getUserByUserCode(int log) {	// log : userCode
+		for(User user : this.list) {
+			if(user.getUserCode() == this.list.get(log).getUserCode())
+				return user;
+		}
+		return null;
+	}
+	
+	public ArrayList<User> getList() {
+		return (ArrayList<User>) this.list.clone();
 	}
 	
 	private boolean duplId(String id) {
 		boolean dupl = false;
 		for(User user : this.list) {
-			if(user.getId().equals(id)) {
+			if(user.getId().equals(id))
 				dupl = true;
-			}
 		}
 		return dupl;
 	}
-
+	
 	private int generateRandomCode() {
 		int code = 0;
+		
 		while(true) {
-			code = (int)(Math.random()*9000) + 1000;
+			code = (int)(Math.random() * 9000) + 1000;
+			
 			boolean dupl = false;
 			for(User user : this.list) {
-				if(user.getUserCode() == code) {
+				if(user.getUserCode() == code) 
 					dupl = true;
-				}
 			}			
-			if(!dupl) {
+			if(!dupl)
 				break;
-			}
 		}		
 		return code;
+	}
+
+	public void loginUser() {		
+		System.out.print("id : ");
+		String id = Atm.scanner.next();
+		System.out.print("password : ");
+		String password = Atm.scanner.next();	
+		
+		for(int i = 0; i < this.list.size(); i++) {
+			
+			String chkId = this.list.get(i).getId();
+			String chkPw = this.list.get(i).getPassword();
+			
+			if(id.equals(chkId) && password.equals(chkPw)) {
+				atm.setLog(i);
+			}
+		}
+		if(atm.getLog()!= -1) {
+			System.out.printf("%s님 환영합니다\n", this.list.get(new Atm().getLog()).getId());
+		}else {
+			System.out.println("로그인 실패");
+		}
+	}
+
+	public void logoutUser() {
+		if(isLogin()) {
+			atm.setLog(-1);
+			System.out.println("로그아웃 성공");
+		}
 	}
 
 	public void leaveUser() {
 		if(isLogin()) {
 			System.out.print("password : ");
-			String password = Atm.scan.next();
+			String password = Atm.scanner.next();
 			
-			String userPassword = this.list.get(this.log).getPassword();
-			if(userPassword.equals(password)) {
-				this.list.remove(this.log);
-				System.out.println("회원탈퇴 완료");
-			}else {
-				System.out.println("비밀번호를 다시 확인하세요.");
+			String chkPw = this.list.get(atm.getLog()).getPassword();
+			if(chkPw.equals(password)) {
+				this.list.remove(atm.getLog());
+				accManager.deleteAcc();
 			}
 		}
 	}
 
-	private boolean isLogin() {		
-		return this.log == -1 ? false : true;
+	private boolean isLogin() {
+		return atm.getLog() == -1 ? false : true;
 	}
-
-	public void loginUser() {
-		if(this.list.size() > 0) {
-			System.out.print("id : ");
-			String id = Atm.scan.next();
-			System.out.print("password : ");
-			String password = Atm.scan.next();		
-			
-			for(int i = 0; i < this.list.size(); i++) {
-				String chkId = this.list.get(i).getId();
-				String chkPw = this.list.get(i).getPassword();
-				if(chkId.equals(id) && chkPw.equals(password)) {
-					this.log = i;
-				}
-			}
-			if(this.log != -1) {
-				System.out.printf("%s님 환영합니다.\n", this.list.get(log).getId());
-			}else {
-				System.out.println("회원정보를 다시 확인하세요.");
-			}
-		}
-	}
-	
-	public void logoutUser() {
-		if(isLogin()) {
-			this.log = -1;
-			System.out.println("로그아웃 성공");
-		}
-
-	}
-
-	public ArrayList<User> getList() {		
-		return (ArrayList<User>) this.list.clone();
-	}
-	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
